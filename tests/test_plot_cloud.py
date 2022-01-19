f3e1b5f4d56068892a8e0487f068e6af767d6e5f
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-19 07:52:16 -0800

    Add test_no_lyrics

--- tests/test_plot_cloud.py
+++ tests/test_plot_cloud.py
@@ -20,42 +20,45 @@ def test_input_types(correct_values):
     with pytest.raises(TypeError):
         _ , file_path = correct_values
         wrong_song_type = ["22", "Taylor Swift"]
-        pc.plot_words(wrong_song_type, file_path)  # should be dict
+        pc.plot_cloud(wrong_song_type, file_path)  # should be dict
 
     with pytest.raises(TypeError):
         song , _ = correct_values
         wrong_file_path = 2
-        pc.plot_words(song, wrong_file_path)  # should be str
+        pc.plot_cloud(song, wrong_file_path)  # should be str
 
     with pytest.raises(TypeError):
         song , file_path = correct_values
-        pc.plot_words(song , file_path, background_color=23)  # should be str
+        pc.plot_cloud(song , file_path, background_color=23)  # should be str
 
     with pytest.raises(TypeError):
         song , file_path = correct_values
-        pc.plot_words(song , file_path, max_font_size=True)  # should be int
+        pc.plot_cloud(song , file_path, max_font_size=True)  # should be int
 
     with pytest.raises(TypeError):
         song , file_path = correct_values
-        pc.plot_words(song , file_path, max_words=True)  # should be int
+        pc.plot_cloud(song , file_path, max_words=True)  # should be int
 
     with pytest.raises(TypeError):
         song , file_path = correct_values
-        pc.plot_words(song , file_path, show="yes")  # should be boolean
+        pc.plot_cloud(song , file_path, show="yes")  # should be boolean
  
 
-# Case 2 - show True or False
 def test_show_true():
+    """Testing the branching of show argument"""
     # How can I get the interim output of a function (plot to screen)?
     assert isinstance(fig, ...), ""
 
 
-# Case 3 - no_lyrics found
-def test_no_lyrics():
+def test_no_lyrics(correct_values):
+    """Testing the case when no lyrics is found"""
+    with pytest.raises(ValueError):
+        _ , file_path = correct_values
+        song = {"22": "Queen"}
+        pc.plot_cloud(song, file_path)
 
 
-
-# Case 4 - image printed
 def test_no_image():
+    """Testing if the printed image is correct"""
     fig = pc.plot_cloud(wrong_song_type, file_path)
-    assert isinstance(fig, ...), "Invalid image type"
+    assert isinstance(fig, plt...), "Invalid image type"
