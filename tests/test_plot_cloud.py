5a8692e5bb6d240649aba40899ebe02c610cb1d6
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-19 08:39:22 -0800

    Add test_image and test_show_True

--- tests/test_plot_cloud.py
+++ tests/test_plot_cloud.py
@@ -1,15 +1,14 @@
+from asyncore import file_dispatcher
 from pylyrics import plot_cloud as pc
+import matplotlib as plt
 import pytest
 import os
 
 
-
 @pytest.fixture
 def correct_values():
-    song = {"22": "Taylor Swift",
-            "Bohemian Rhapsody": "Queen"    
-            }
-    file_path = os.getcwd() + "/tests/data/22.jpg"
+    song = {"22": "Taylor Swift", "Bohemian Rhapsody": "Queen"}
+    file_path = os.getcwd() + "/tests/data/22"
 
     return song, file_path
 
@@ -18,47 +17,50 @@ def test_input_types(correct_values):
     """Test wrong input types"""
 
     with pytest.raises(TypeError):
-        _ , file_path = correct_values
+        _, file_path = correct_values
         wrong_song_type = ["22", "Taylor Swift"]
         pc.plot_cloud(wrong_song_type, file_path)  # should be dict
 
     with pytest.raises(TypeError):
-        song , _ = correct_values
+        song, _ = correct_values
         wrong_file_path = 2
         pc.plot_cloud(song, wrong_file_path)  # should be str
 
     with pytest.raises(TypeError):
-        song , file_path = correct_values
-        pc.plot_cloud(song , file_path, background_color=23)  # should be str
+        song, file_path = correct_values
+        pc.plot_cloud(song, file_path, background_color=23)  # should be str
 
     with pytest.raises(TypeError):
-        song , file_path = correct_values
-        pc.plot_cloud(song , file_path, max_font_size=True)  # should be int
+        song, file_path = correct_values
+        pc.plot_cloud(song, file_path, max_font_size=True)  # should be int
 
     with pytest.raises(TypeError):
-        song , file_path = correct_values
-        pc.plot_cloud(song , file_path, max_words=True)  # should be int
+        song, file_path = correct_values
+        pc.plot_cloud(song, file_path, max_words=True)  # should be int
 
     with pytest.raises(TypeError):
-        song , file_path = correct_values
-        pc.plot_cloud(song , file_path, show="yes")  # should be boolean
- 
+        song, file_path = correct_values
+        pc.plot_cloud(song, file_path, show="yes")  # should be boolean
+
 
 def test_show_true():
-    """Testing the branching of show argument"""
-    # How can I get the interim output of a function (plot to screen)?
-    assert isinstance(fig, ...), ""
+    """Testing the branching of show argument to see if image is generated"""
+    song, file_path = correct_values
+    fig = pc.plot_cloud(song, file_path, show=True)
+    assert isinstance(fig, plt.figure), "No plot has been printed to screen"
 
 
 def test_no_lyrics(correct_values):
     """Testing the case when no lyrics is found"""
     with pytest.raises(ValueError):
-        _ , file_path = correct_values
+        _, file_path = correct_values
         song = {"22": "Queen"}
         pc.plot_cloud(song, file_path)
 
 
-def test_no_image():
-    """Testing if the printed image is correct"""
-    fig = pc.plot_cloud(wrong_song_type, file_path)
-    assert isinstance(fig, plt...), "Invalid image type"
+def test_image(correct_values):
+    """Testing if an image is saved"""
+    song, file_path = correct_values
+    pc.plot_cloud(song, file_path)
+    fig = open(file_path + ".png").read()
+    assert fig == True, "No image has been saved"
