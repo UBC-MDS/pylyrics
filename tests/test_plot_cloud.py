ed758c94711817a2319ad4b69d4b7a3f0f6135a3
Artan Zandian <74034506+artanzand@users.noreply.github.com>
2022-01-18 22:32:47 -0800

    Add test_input_types

--- tests/test_plot_cloud.py
+++ tests/test_plot_cloud.py
@@ -1,14 +1,47 @@
 from pylyrics import plot_cloud as pc
 import pytest
+import os
 
-# Case 1 - wrong input types
-def test_input_types():
-    try:
+
+
+@pytest.fixture
+def correct_values():
+    song = {"22": "Taylor Swift",
+            "Bohemian Rhapsody": "Queen"    
+            }
+    file_path = os.getcwd() + "/tests/data/22.jpg"
+
+    return song, file_path
+
+
+def test_input_types(correct_values):
+    """Test wrong input types"""
+
+    with pytest.raises(TypeError):
+        _ , file_path = correct_values
         wrong_song_type = ["22", "Taylor Swift"]
-        file_path = "image.png"
-        assert pc.plot_cloud(wrong_song_type, file_path) == ValueError
+        pc.plot_words(wrong_song_type, file_path)  # should be dict
 
+    with pytest.raises(TypeError):
+        song , _ = correct_values
+        wrong_file_path = 2
+        pc.plot_words(song, wrong_file_path)  # should be str
 
+    with pytest.raises(TypeError):
+        song , file_path = correct_values
+        pc.plot_words(song , file_path, background_color=23)  # should be string
+
+    with pytest.raises(TypeError):
+        song , file_path = correct_values
+        pc.plot_words(song , file_path, max_font_size=True)  # should be int
+
+    with pytest.raises(TypeError):
+        song , file_path = correct_values
+        pc.plot_words(song , file_path, max_words=True)  # should be int
+
+    with pytest.raises(TypeError):
+        song , file_path = correct_values
+        pc.plot_words(song , file_path, show="yes")  # should be boolean
  
 
 # Case 2 - show True or False
@@ -24,5 +57,5 @@ def test_no_lyrics():
 
 # Case 4 - image printed
 def test_no_image():
-    with pc.plot_cloud(wrong_song_type, file_path) as fig:
-        assert isinstance(fig, ...), "Invalid image type"
+    fig = pc.plot_cloud(wrong_song_type, file_path)
+    assert isinstance(fig, ...), "Invalid image type"
