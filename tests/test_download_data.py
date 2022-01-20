5bf54ad29691358e26745516cdb43d65366d2f4c
manju-abhinandana <manju.na87@gmail.com>
2022-01-20 01:53:32 -0800

    feat: updated test for input types

--- tests/test_download_data.py
+++ tests/test_download_data.py
@@ -1,19 +1,43 @@
-from pylyrics import download_data
+from pylyrics import download_data as dd
 import pandas as pd
 import pytest
 import os
 
+def test_input_types():
+    """Test for input types"""
+
+    with pytest.raises(TypeError):
+        file_path = os.getcwd() + "/tests/data/"
+        columns = ["song_title","artist"]
+        dd.download_data(1, "file_path", columns)  # dataset: should be string
+
+    with pytest.raises(TypeError):
+        dataset = "geomack/spotifyclassification"
+        columns = ["song_title","artist"]
+        dd.download_data(dataset, 1, columns)  # filepath: should be string
+
+    with pytest.raises(TypeError):
+        dataset = "geomack/spotifyclassification"
+        file_path = os.getcwd() + "/tests/data/"
+        columns = "song_title"
+        dd.download_data(dataset, "file_path", columns)  # columns: should be a list
+        
+    with pytest.raises(TypeError):
+        dataset = "geomack/spotifyclassification"
+        file_path = os.getcwd() + "/tests/data/"
+        columns = ["song_title"]
+        dd.download_data(dataset, "file_path", columns)  # columns: should be a list of length 2  
 
 def test_dataframe():
-    assert isinstance(download_data("geomack/spotifyclassification",
-                              "data/spotify_attributes", 
-                              ["song_title", "artist"]),pd.DataFrame) == True, "Should return a dataframe"
+    dataset = "geomack/spotifyclassification"
+    filepath = "tests/data/spotify_attributes"
+    columns = ["song_title", "artist"]
+    # assert isinstance(download_data(dataset, filepath, columns),pd.DataFrame) == True, "Should return a dataframe"
+    assert len(dd.download_data(dataset, filepath, columns).columns) == 2, "Should return a dataframe with 2 columns"
     
-    assert len(download_data("geomack/spotifyclassification",
-                              "data/spotify_attributes", 
-                              ["artist", "song_title"]).columns) == 2, "Should return a dataframe with 2 columns"
-    
-    assert set(("artist", "song_title")).issubset(
-        download_data("geomack/spotifyclassification",
-                              "data/spotify_attributes", 
-                              ["artist", "song_title"]).columns) == True, "Columns names are incorrect"
+def test_output():
+    with pytest.raises(ValueError):
+        dataset = "geomack/spotifyclassification"
+        filepath = "tests/data/spotify_attributes"
+        columns = ["song", "artist"]
+        dd.download_data(dataset, filepath, columns) # Columns names are incorrect
