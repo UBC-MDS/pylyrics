bab2e5549188ed837c556e3a60fda24f9561c4ef
Abhiket Gaurav <p12abhiketg@iimidr.ac.in>
2022-01-20 13:08:13 -0800

    add test_clean_text

--- src/pylyrics/clean_text.py
+++ src/pylyrics/clean_text.py
@@ -2,11 +2,11 @@
 # January 2022
 
 import re
-from pylyrics import extract_lyrics
+# from extract_lyrics import extract_lyrics
 
 
 def clean_text(text):
-    """Cleans the text by removing special characters, html_tags, #tags, contaction words.
+    """Cleans the text by removing special characters, html_tags, #tags, contaction words and convert everything to lower case.
 
     Parameters
     ----------
@@ -23,18 +23,18 @@ def clean_text(text):
     >>> clean_text("Early optimization is the root of all evil!")
     'early optimization is the root of all evil'
     """
-    ## Exception Handeling to be added
+    
+    # check input types
+    if type(text) != str:
+        raise TypeError("Text should be a variable of type string.")
 
-    contra_dict={"'s":" is","n't":" not","'m":" am","'ll":" will",
-           "'d":" would","'ve":" have","'re":" are"}
-    #replace the contractions
+    contra_dict={"'s":" is","n't":" not","'m":" am","'ll":" will", "'d":" would","'ve":" have","'re":" are"}
     for key,value in contra_dict.items():
         if key in text:
             text = text.replace(key,value)
-    # lower case and remove special characters
+        # lower case and remove special characters
     text = re.sub(r'[^a-zA-Z\s]', '', text, re.I|re.A)
     text = re.sub(r'https?:\/\/.\S+', "", text)
     text = re.sub(r'#', '', text)
     text = text.lower()
-    
     return text
---
+++ tests/test_clean_text.py
@@ -0,0 +1,49 @@
+# Authors: Abhiket Gaurav, Artan Zandian, Macy Chan, Manju Abhinandana Kumar
+# January 2022
+
+from pylyrics import clean_text as ct
+import re
+
+# Case 1
+def test_case1():
+    lyrics = "It feels like a perfect night To dress up like hipsters And make fun of our exes"
+    target = "it feels like a perfect night to dress up like hipsters and make fun of our exes"
+    assert (
+        ct.clean_text(lyrics) == target
+    ), "Something went wrong"
+
+# Case 2 - empty text
+def test_case2():
+    try:
+        lyrics = " "
+        assert (
+            len(ct.clean_text(lyrics).strip()) != 0
+        ), "Blank text input"
+    except:
+        assert True
+test_case1()
+test_case2()
+
+# Case 3 - wrong type
+def test_case3():
+    try:
+        lyrics = "!!I Love you"
+        with pytest.raises(TypeError):
+            regex = re.compile('[@_!#$%^&*()<>?/|}{~:]')
+            subtext = ct.clean_text(lyrics)[0:1]
+        assert (
+            regex.search(subtext) != None
+        ), "Text cannot start with special character"
+    except:
+        assert True
+
+
+# Case 4 - small text
+def test_case4():
+    try:
+        lyrics = "!!I Love you"
+        assert (
+            len(ct.clean_text(lyrics)) <=5 
+        ), "Too small to give meaniful output"
+    except:
+        assert True
